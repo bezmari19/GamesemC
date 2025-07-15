5d0ae6ee720dfd4271acad75bf2271e1dde26b06
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-15 17:01:06 -0300

    :tada::books: commit init primeiro game

---
+++ Quizzes/QuizAtualidades.c
@@ -0,0 +1,35 @@
+// Primeiro programa em C de Quizzes
+
+#include <stdio.h>
+
+int main() {
+
+// Mensagem de boas-vindas
+
+printf("/*\n");
+printf(" ______     __  __     __     ______     ______    \n");
+printf("/\\  __ \\   /\\ \\/\\ \\   /\\ \\   /\\___  \\   /\\___  \\   \n");
+printf("\\ \\ \\/\\_\\  \\ \\ \\_\\ \\  \\ \\ \\  \\/_/  /__  \\/_/  /__  \n");
+printf(" \\ \\___\\_\\  \\ \\_____\\  \\ \\_\\   /\\_____\\   /\\_____\\ \n");
+printf("  \\/___/_/   \\/_____/   \\/_/   \\/_____/   \\/_____/ \n");
+printf("*/\n");
+
+printf("****************************************\n");
+printf("Seja bem-vindo ao Quiz de Atualidades!\n");
+printf("Aqui você poderá testar seus conhecimentos sobre os principais acontecimentos do mundo.\n");
+printf("****************************************\n");
+printf("Este quiz foi desenvolvido por: Marina Ferraz\n");
+
+// Menu do jogo:
+printf("****************************************\n");
+printf("Aqui é o menu do jogo!\n");
+printf("****************************************\n");
+printf("Escolha uma das opções abaixo:\n");
+printf("[1] Iniciar o quiz!\n");
+printf("[2] Regras\n");
+printf("[3] Sair do jogo\n");
+
+
+
+
+}
