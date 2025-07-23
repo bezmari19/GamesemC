2a0129ea4bfce88afc29c0f228a6b43334285043
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-23 16:24:49 -0300

    :books:docs: em progresso

--- Quizzes/QuizAtualidades.c
+++ Quizzes/QuizAtualidades.c
@@ -29,6 +29,52 @@ printf("[1] Iniciar o quiz!\n");
 printf("[2] Regras\n");
 printf("[3] Sair do jogo\n");
 
+int opcao_de_jogo;
+printf("Digite a opção que você deseja:\n");
+scanf("%d", &opcao_de_jogo);
+
+if (opcao_de_jogo == 1) {
+    printf("Você escolheu a opção 1: Iniciar o quiz!\n");
+    printf("*****************************************\n");
+    printf("Prepare-se para responder as 20 perguntas!\n");
+    printf("Você deseja continuar?(S para sim e N para não)\n");
+    char continuar;
+    scanf("%c", &continuar);
+    if (continuar == "S" || continuar == "s") {
+        printf("Ótimo! Vamos começar então.\n");
+    } else if (continuar == "N" || continuar == "n") {
+        printf("Tudo bem! Vamos voltar ao menu.\n");
+    } else {
+        printf("Opção inválida! Vamos voltar ao menu.\n");
+    }
+
+    printf("*****************************************\n");
+    printf("O quizz será iniciado agora!\n");
+    printf("Primeira pergunta:\n");
+
+    printf("");
+}
+
+if (opcao_de_jogo == 2) {
+    printf("**************************************************\n");
+    printf("Regras do Quiz de Atualidades:\n");
+    printf("1- O quizz consiste em perguntar de multipla escolha sobre atualidade mundiais.\n");
+    printf("2- O jogador deve escolher a alternativa correta.\n");
+    printf("3- Cada resposta correta vale 1 ponto. A cada certo, 1 ponto é adicionado a soma total.\n");
+    printf("4- O jogo acaba após as 20 perguntas serem respondidas.\n");
+    printf("5- Caso o jogador erre, é descontado um ponto da soma total de acertos.\n");
+    printf("6 - O jogador não acumula acertos a cada partida jogada. É resetado a cada nova partida.\n");
+    printf("7 - O jogador pode sair do jogo a qualquer momento. O jogo não será salvo.\n");
+    printf("**************************************************\n");
+}
+
+if (opcao_de_jogo == 3) {
+    printf("**************************************************\n");
+    printf("Obrigado por jogar o Quiz de Atualidades!\n");
+    printf("Esperamos que tenha se divertido e aprendido algo novo.\n");
+    printf("Volte sempre para testar seus conhecimentos!\n");
+    printf("**************************************************\n");
+    return 0; // Sair do jogo
 
 
 
